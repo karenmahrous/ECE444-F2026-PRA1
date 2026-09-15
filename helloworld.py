41411ffadeed139ec8e81e46f93dc119824f4755
Karen <karen.mahrous@mail.utoronto.ca>
2026-09-15 00:51:50 -0400

    c1

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,7 @@
-
-   print('Hello, my name is Karen, I have been at UofT for 2 years')
+<<<<<<< HEAD
+print('Hello World, my name is Karen, I have been at UofT for 3 years')
+=======
+#testing rebase c1
+print('my name is Karen, I have been at UofT for 3 years')
+>>>>>>> c77db21 (c2)
 
