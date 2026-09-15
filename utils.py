54ee73b4f2ee2650a6058ebe9ac0244fa9ee3814
Karen <karen.mahrous@mail.utoronto.ca>
2026-09-15 00:37:15 -0400

    Add utils class with reversed and formatter functions

---
+++ utils.py
@@ -0,0 +1,13 @@
+   class utils:
+       @staticmethod
+       def reversed(number):
+           if not isinstance(number, int):
+               raise TypeError("Input must be an integer")
+           sign = -1 if number < 0 else 1
+           return sign * int(str(abs(number))[::-1])
+
+       @staticmethod
+       def formatter(number):
+           if not isinstance(number, int):
+               raise TypeError("Input must be an integer")
+           return bin(number), oct(number)
